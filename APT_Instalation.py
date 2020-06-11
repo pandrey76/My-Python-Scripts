1d33b6a02881f0a313eb63dc40a569be4eabdb13
pandrey76 <pandrey76@yandex.ru>
2020-06-11 11:49:29 +0300

    Added APT_Instalation.py file.

---
+++ APT_Instalation.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#-------------------------------------------------------------------------------
+# Name:        модуль1
+# Purpose:
+#
+# Author:      Prapor
+#
+# Created:     05.12.2017
+# Copyright:   (c) Prapor 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import subprocess
+import os
+
+#git clone "" ""
+
+
+#os.system(r"Notepad.exe")
+
+#ip = "localhost"
+#proc = subprocess.Popen("ping -c2 %s" % ip, shell = True, stdout = subprocess.PIPE)
+proc = subprocess.Popen("Notepad.exe", shell = False, stdout = subprocess.PIPE)
+
+
+#out = proc.stdout.readlines()
+#out = proc.communicate()
+#proc.wait(10)
+#proc.terminate()
+
+#proc.kill()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
