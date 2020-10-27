b158da9a78d41c1974be5e97ad042c054de6c36d
pandrey76 <pandrey76@yandex.ru>
2020-10-27 11:42:14 +0300

    Edited ConstructOneFileFromMany.py file.
    Added codec support.

--- ConstructOneFileFromMany.py
+++ ConstructOneFileFromMany.py
@@ -2,31 +2,38 @@
 # -*- coding: UTF-8 -*-
 import os
 from sys import argv, exit
+import codecs
 
 
+def construct_big_file_from_many_little(path, name, ext):
 
-def construc_big_file_from_many_little(path, name, ext):
-	root, dirs, files = os.walk(path)
-	with open(os.path.join(path, name) + '.' + ext, 'w') as result_file:
-		count = 0
-		while count <= files.len():
-			try:
-				full_file_path = os.path.join(path, str(count)) + '.' + ext;
-				with open(full_file_path, 'r') as f:
-					print (full_file_path)
-					file_content = f.read()
-					
-					marker = "#################"
-					result_file.write(marker)
-					result_file.write(str(count) + '.' + ext)
-					result_file.write(marker)
-					result_file.write(file_content)
-					result_file.write(marker + marker)
-					
-					
-			except FileNotFoundError(ex):
-				print(ex)
-			count = count + 1
+	for root, dirs, files in os.walk(path):
+		with codecs.open(os.path.join(path, name) + '.' + ext, 'w', "utf-8") as result_file:
+			count = 0
+			while count <= len(files):
+				try:
+					full_file_path = os.path.join(path, str(count)) + '.' + ext
+					with codecs.open(full_file_path, 'r', "cp1251") as f:
+						print(full_file_path)
+						file_content = f.read()
+
+						marker = "#################"
+						result_file.write(os.linesep)
+						result_file.write(marker)
+						result_file.write('    ')
+						result_file.write(str(count) + '.' + ext)
+						result_file.write('    ')
+						result_file.write(marker)
+						result_file.write(os.linesep)
+						result_file.write(file_content)
+						result_file.write(os.linesep)
+						result_file.write(marker + marker)
+						result_file.write(os.linesep)
+				except FileNotFoundError as ex:
+					print(ex)
+				# except Exception as ex:
+				# 	print(ex)
+				count = count + 1
 
 
 if __name__ == '__main__':
@@ -41,7 +48,8 @@ if __name__ == '__main__':
 		print("Слишком много параметров")
 		exit(0)
 	
-	construc_big_file_from_many_little(argv[1], argv[2], argv[3])  
+# Example: ConstructOneFileFromMany.py "/home/admin1/work/temp/" S-1-5-21-2772757386-1096924173-415189654-1002 reg
+	construct_big_file_from_many_little(argv[1], argv[2], argv[3])
 	
 				
 		
